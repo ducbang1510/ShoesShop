a84ce3611256c9b3ab2a242997f1e69d70be4c8a
Văn Trung <trung.pv194@gmail.com>
2021-09-20 11:20:23 +0700

    Add manage employees function

--- ShoesShop/BUS/BUS_NhanVien.cs
+++ ShoesShop/BUS/BUS_NhanVien.cs
@@ -3,10 +3,59 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using ShoesShop.DAO;
 
 namespace ShoesShop.BUS
 {
     class BUS_NhanVien
     {
+        DAO_NhanVien daoNV;
+        
+        public BUS_NhanVien()
+        {
+            daoNV = new DAO_NhanVien();
+        }
+
+        public void LayDSNhanVien(DataGridView dg)
+        {
+            dg.DataSource = daoNV.LayDSNhanVien();
+        }
+
+        public void ThemNhanVien(Employee nv)
+        {
+            if (daoNV.ThemNhanVien(nv))
+            {
+                MessageBox.Show("Thêm nhân viên mới thành công");
+            }
+            else
+            {
+                MessageBox.Show("Thêm nhân viên mới thất bại");
+            }
+        }
+
+        public void SuaThongTinNhanVien(Employee nv)
+        {
+            if (daoNV.SuaThongTinNhanVien(nv))
+            {
+                MessageBox.Show("Sửa thông tin nhân viên thành công");
+            }
+            else
+            {
+                MessageBox.Show("Sửa thông tin nhân viên thất bại");
+            }
+        }
+
+        public void XoaNhanVien(int maNV)
+        {
+            if (daoNV.XoaNhanVien(maNV))
+            {
+                MessageBox.Show("Xóa nhân viên thành công");
+            }
+            else
+            {
+                MessageBox.Show("Xóa nhân viên thất bại");
+            }
+        }
     }
 }
--- ShoesShop/FQuanLyNhanVien.cs
+++ ShoesShop/FQuanLyNhanVien.cs
@@ -7,14 +7,100 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ShoesShop.BUS;
 
 namespace ShoesShop
 {
     public partial class FQuanLyNhanVien : Form
     {
+        BUS_NhanVien busNV;
+
         public FQuanLyNhanVien()
         {
             InitializeComponent();
+            busNV = new BUS_NhanVien();
+        }
+
+        public void HienThiDSNhanVien()
+        {
+            CapNhatForm();
+            dGVNhanVien.DataSource = null;
+            busNV.LayDSNhanVien(dGVNhanVien);
+        }
+
+        public void CapNhatForm()
+        {
+            txtMaNV.Text = "";
+            txtHoTen.Text = "";
+            txtGioiTinh.Text = "";
+            txtEmail.Text = "";
+            txtSDT.Text = "";
+            txtDiaChi.Text = "";
+            txtVaiTro.Text = "";
+            txtTaiKhoan.Text = "";
+            txtMatKhau.Text = "";
+        }
+
+        private void FQuanLyNhanVien_Load(object sender, EventArgs e)
+        {
+            HienThiDSNhanVien();
+        }
+
+        private void btThem_Click(object sender, EventArgs e)
+        {
+            Employee nv = new Employee();
+            nv.EmployeeID = int.Parse(txtMaNV.Text);
+            nv.FullName = txtHoTen.Text;
+            nv.DateOfBirth = dtpNgaySinh.Value.Date;
+            nv.Gender = txtGioiTinh.Text;
+            nv.Email = txtEmail.Text;
+            nv.Phone = txtSDT.Text;
+            nv.Address = txtDiaChi.Text;
+            nv.UserRole = txtVaiTro.Text;
+            nv.Username = txtTaiKhoan.Text;
+            nv.Password = txtMatKhau.Text;
+
+            busNV.ThemNhanVien(nv);          
+            HienThiDSNhanVien();
+        }
+
+        private void btSua_Click(object sender, EventArgs e)
+        {
+            Employee nv = new Employee();
+            nv.EmployeeID = int.Parse(txtMaNV.Text);
+            nv.FullName = txtHoTen.Text;           
+            nv.Email = txtEmail.Text;
+            nv.Phone = txtSDT.Text;
+            nv.Address = txtDiaChi.Text;            
+            nv.Username = txtTaiKhoan.Text;
+            nv.Password = txtMatKhau.Text;
+
+            busNV.SuaThongTinNhanVien(nv);
+            HienThiDSNhanVien();
+        }
+
+        private void btXoa_Click(object sender, EventArgs e)
+        {
+            int maNV = int.Parse(txtMaNV.Text);
+            busNV.XoaNhanVien(maNV);
+            HienThiDSNhanVien();
+        }
+
+        private void dGVNhanVien_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0 && e.RowIndex < dGVNhanVien.Rows.Count)
+            {
+                txtMaNV.Text = dGVNhanVien.Rows[e.RowIndex].Cells["EmployeeID"].Value.ToString();
+                txtHoTen.Text = dGVNhanVien.Rows[e.RowIndex].Cells["FullName"].Value.ToString();
+                txtGioiTinh.Text = dGVNhanVien.Rows[e.RowIndex].Cells["Gender"].Value.ToString();
+                dtpNgaySinh.Text = dGVNhanVien.Rows[e.RowIndex].Cells["DateOfBirth"].Value.ToString();
+                txtEmail.Text = dGVNhanVien.Rows[e.RowIndex].Cells["Email"].Value.ToString();
+                txtSDT.Text = dGVNhanVien.Rows[e.RowIndex].Cells["Phone"].Value.ToString();
+                txtDiaChi.Text = dGVNhanVien.Rows[e.RowIndex].Cells["Address"].Value.ToString();
+                txtVaiTro.Text = dGVNhanVien.Rows[e.RowIndex].Cells["UserRole"].Value.ToString();
+                txtTaiKhoan.Text = dGVNhanVien.Rows[e.RowIndex].Cells["UserName"].Value.ToString();
+                txtMatKhau.Text = dGVNhanVien.Rows[e.RowIndex].Cells["Password"].Value.ToString();
+            }
         }
     }
 }
